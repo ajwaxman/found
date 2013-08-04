c54512a356547f4994b8b6bbbc9f64194bb06fd5
Adam Waxman <awaxman11@gmail.com>
2013-08-04 14:25:58 -0400

    Fix avail_on_weekend column in landlord model

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -5,19 +5,20 @@ end
 #
 # Table name: landlords
 #
-#  id             :integer         not null, primary key
-#  name           :string(255)
-#  email          :string(255)
-#  phone          :string(255)
-#  neighborhood   :string(255)
-#  hours_avail_am :integer
-#  hours_avail_pm :integer
-#  internet       :string(255)
-#  desk_seats     :integer
-#  total_seats    :integer
-#  price          :integer
-#  amenities      :string(255)
-#  created_at     :datetime        not null
-#  updated_at     :datetime        not null
+#  id                :integer         not null, primary key
+#  name              :string(255)
+#  email             :string(255)
+#  phone             :string(255)
+#  neighborhood      :string(255)
+#  hours_avail_am    :integer
+#  hours_avail_pm    :integer
+#  internet          :string(255)
+#  desk_seats        :integer
+#  total_seats       :integer
+#  price             :integer
+#  amenities         :string(255)
+#  created_at        :datetime        not null
+#  updated_at        :datetime        not null
+#  avail_on_weekends :string(255)
 #
 
